160312954f05b38d23e4148bb179872e71be021f
Kevin3719 <36079138+Kevin3719@users.noreply.github.com>
2018-09-20 14:10:34 -0400

    Added Test Cases

--- CtoFTester.java
+++ CtoFTester.java
@@ -2,6 +2,16 @@ public static double CtoF(double degrees){
    return (degrees - 32) * 9.0  / 5.0;
 }
 
-public static double FtoC(double degrees){
+public static double  FtoC(double degrees){
    return degree * 5.0/9.0 + 32;
+}
+public static void Testing() {
+  System.out.print FtoC(3);
+  System.out.print FtoC(4);
+  System.out.print FtoC(5);
+  System.out.print FtoC(6);
+  System.out.print CtoF(3);
+  System.out.print CtoF(4);
+  System.out.print CtoF(5);
+  System.out.print CtoF(6);
 }
