884642e41c4b815feac3f9b1afd817281633feb8
Kevin3719 <36079138+Kevin3719@users.noreply.github.com>
2018-09-20 14:06:32 -0400

    Fixed code, Realize in Class

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,7 +1,7 @@
 public static double CtoF(double degrees){
-   return (degrees - 32) * 9  / 5;
+   return (degrees - 32) * 9.0  / 5.0;
 }
 
 public static double FtoC(double degrees){
-   return 5/9 * degrees + 32;
+   return degree * 5.0/9.0 + 32;
 }
